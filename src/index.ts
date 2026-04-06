4919477f3000990c4381644a4bca3347f914afee
root <root@srv1462158.hstgr.cloud>
2026-04-06 15:31:05 +0000

    Disable Telegram bot

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ dotenv.config();
 
 import http from "http";
 import app, { attachWebSocket } from "./platforms/server";
-import "./platforms/telegram";
 
 const PORT = process.env.PORT ?? 3001;
 const server = http.createServer(app);
