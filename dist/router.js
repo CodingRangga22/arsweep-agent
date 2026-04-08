3345e1a20964a254f0cc26a05e4ac0bac05649dd
CodingRangga22 <rsptr492@gmail.com>
2026-04-08 07:41:36 +0700

    chore: remove node_modules and dist from git

--- dist/router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleMessage = handleMessage;
-const core_1 = require("./agent/core");
-async function handleMessage(input) {
-    console.log(`[Router] ${input.platform} | user:${input.userId} | "${input.message.slice(0, 60)}"`);
-    const result = await (0, core_1.runAgent)(`${input.platform}:${input.userId}`, input.message, input.walletAddress);
-    return { ...result, platform: input.platform, userId: input.userId };
-}
